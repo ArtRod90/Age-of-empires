8dd77271a963a0e98fc125c4b2fc410621c1b13f
ArtRod90 <arturorod90artica@gmail.com>
2022-05-14 21:08:36 -0500

    UpDate

--- includes/app.php
+++ includes/app.php
@@ -9,7 +9,7 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
-require 'database.php';
+// require 'database.php';
 // Conectarnos a la base de datos
-use Model\ActiveRecord;
-ActiveRecord::setDB($db);
+// use Model\ActiveRecord;
+// ActiveRecord::setDB($db);
